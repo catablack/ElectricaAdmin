9c5557c8e0acb51114e90c8760ca14c8794d4620
Cata Pop <catalin_pop12@yahoo.com>
2018-03-20 19:41:33 +0200

    commit Test

--- src/main/java/ElectricaAdminMV/App.java
+++ src/main/java/ElectricaAdminMV/App.java
@@ -1,13 +1,17 @@
 package ElectricaAdminMV;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.io.BufferedWriter;
+import ElectricaAdminMV.repository.DataManager;
+import ElectricaAdminMV.controller.ClientController;
+import ElectricaAdminMV.ui.ElectricaUI;
+
+public class App {
+	public static void main(String[] args) {
+		DataManager repo = new DataManager();
+		
+		ClientController ctrl = new ClientController();
+		
+		ElectricaUI console = new ElectricaUI(ctrl);
+		console.Run();
+	}
 }
---
+++ src/test/java/ElectricaAdminMV/repository/DataManagerTest.java
@@ -0,0 +1,18 @@
+package ElectricaAdminMV.repository;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by catablack.
+ */
+public class DataManagerTest extends TestCase {
+
+    public void testSaveChanges() {
+    }
+
+    public void testGetInvoicesList() {
+    }
+
+    public void testGetClientsList() {
+    }
+}
