9130877248807e35e9b2cd36679b2a311d23a19f
Cata Pop <catalin_pop12@yahoo.com>
2018-03-20 18:34:02 +0200

    Initial commit

---
+++ src/main/java/ElectricaAdminMV/App.java
@@ -0,0 +1,13 @@
+package ElectricaAdminMV;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
