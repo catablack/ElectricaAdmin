f9137e0d4f984154f4071a4262f481fa4ad801dc
Cata Pop <catalin_pop12@yahoo.com>
2018-04-03 16:11:29 +0300

    assertin test

--- src/test/java/ElectricaAdminMV/repository/DataManagerTest.java
+++ src/test/java/ElectricaAdminMV/repository/DataManagerTest.java
@@ -8,11 +8,14 @@ import junit.framework.TestCase;
 public class DataManagerTest extends TestCase {
 
     public void testSaveChanges() {
+        assert(true);
     }
 
     public void testGetInvoicesList() {
+        assert(true);
     }
 
     public void testGetClientsList() {
+        assert(true);
     }
 }
