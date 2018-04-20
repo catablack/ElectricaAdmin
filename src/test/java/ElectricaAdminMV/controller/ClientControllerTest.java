a78b33ab390ea96a06f8c6327115505a1f3ce088
Cata Pop <catalin_pop12@yahoo.com>
2018-04-20 14:29:51 +0300

    WBT Test case

---
+++ src/test/java/ElectricaAdminMV/controller/ClientControllerTest.java
@@ -0,0 +1,29 @@
+package ElectricaAdminMV.controller;
+
+import ElectricaAdminMV.model.Client;
+import junit.framework.TestCase;
+
+/**
+ * Created by catablack.
+ */
+public class ClientControllerTest extends TestCase {
+    ClientController controller;
+
+    public void setUp() throws Exception {
+        super.setUp();
+        controller = new ClientController();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testAddClient() {
+        controller.AddClient("Test","Sdr","1");
+    }
+
+    public void testAddClientIndex() {
+    }
+
+    public void testListIssue() {
+    }
+}
