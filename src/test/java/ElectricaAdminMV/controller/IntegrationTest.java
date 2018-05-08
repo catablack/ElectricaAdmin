0109162d3497b630879b107b5cc1e810f703d58c
Cata Pop <catalin_pop12@yahoo.com>
2018-05-08 15:23:51 +0300

    IntegrationTest

---
+++ src/test/java/ElectricaAdminMV/controller/IntegrationTest.java
@@ -0,0 +1,58 @@
+package ElectricaAdminMV.controller;
+
+import ElectricaAdminMV.model.Client;
+import ElectricaAdminMV.repository.DataManager;
+import junit.framework.TestCase;
+
+/**
+ * Created by catablack.
+ */
+public class IntegrationTest extends TestCase {
+
+
+    private ClientController controller;
+    private DataManager dm = new DataManager();
+
+    public void setUp() throws Exception {
+        super.setUp();
+        controller = new ClientController();
+    }
+
+    public void testSaveChanges() {
+
+        dm.Clients.add(new Client("Cata", "strada", "1"));
+        assert (dm.Clients.size() > 0);
+
+    }
+
+    public void testAddClient() {
+        controller.AddClient("Test", "Sdr", "1");
+        controller.AddClient("", "Sdr", "1");
+        controller.AddClient("Test", "Sdr", "1");
+
+    }
+
+    public void testFunctionC()
+    {
+        controller.AddClient("FunctionC", "Sdr2", "12");
+        controller.AddClient("", "Sdr2", "12");
+        controller.AddClient("FunctionC", "Sdr2", "12");
+    }
+
+    public void testA()
+    {
+        this.testSaveChanges();
+    }
+    public void testAB()
+    {
+        this.testSaveChanges();
+        this.testAddClient();
+    }
+
+    public void testABC()
+    {
+        this.testSaveChanges();
+        this.testAddClient();
+        this.testFunctionC();
+    }
+}
